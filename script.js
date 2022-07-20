52a7666648055c6cf65e16220969ba1825e5d663
Jefferson-HSL <santoslima963@gmail.com>
2022-07-20 20:34:10 -0300

    last commit

--- script.js
+++ script.js
@@ -1,35 +1,12 @@
 function Convert(){
     const valueInDollar = document.querySelector("input");
-    let filteredValue;
+    let filteredValue = parseInt(valueInDollar.value.replace(/[^0-9]/g, "") * 100);
     
     /* type of coins */
     let quarter = 0;
     let dime = 0;
     let nickel = 0;
     let penny = 0;
-
-    
-    if(valueInDollar.value.indexOf(",") > -1){
-        
-        filteredValue = valueInDollar.value.replace(",", "");
-        console.log("entrou no teste da virgula");
-        
-    }
-
-    else{
-        
-        filteredValue = valueInDollar.value * 100;
-        console.log(`entrou no teste de não ter a virgula: ${filteredValue}`);
-        
-    }
-    
-    if(filteredValue.value.indexOf(".") > -1){
-    
-        filteredValue = filteredValue.value.replace(".", "");
-        console.log("entrou no teste do ponto");
-    
-    }
-
 
     while (filteredValue > 0){
         
